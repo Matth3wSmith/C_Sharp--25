a86d63c22c77a6af67fe2276b53eb9ad40329167
Matthew-F4 <kovacs.mate.22@F4-53>
2025-10-22 11:23:56 +0200

    Emelt digitális kultúra érettségi - Sebesség

---
+++ digikult_erettsegi/sebesseg/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace sebesseg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Ut> utak = new List<Ut>();
+            StreamReader olvas = new StreamReader("ut.txt");
+            int osszUt = int.Parse(olvas.ReadLine());
+            while (!olvas.EndOfStream)
+            {
+                utak.Add(new Ut(olvas.ReadLine()));
+            }
+
+            olvas.Close();  
+
+        }
+    }
+}
---
+++ digikult_erettsegi/sebesseg/Ut.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sebesseg
+{
+    internal class Ut
+    {
+        public int km;
+        public string jelzes;
+        public Ut(string sor)
+        {
+            string[] adatok = sor.Split(' ');
+            this.km = int.Parse(adatok[0]);
+            this.jelzes = adatok[1];
+
+        }
+        public Ut(int ut,string jelzes)
+        {
+            this.km = ut;
+            this.jelzes = jelzes;
+        }
+    }
+}
