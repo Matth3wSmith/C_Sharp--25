a93d44d47fd0f67cc8708a736853a904b84a6f97
Matthew-F4 <kovacs.mate.22@F4-53>
2026-01-13 11:21:31 +0100

    operatorok

---
+++ operatorok_gyak/EgyObj.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operatorok_gyak
+{
+    internal class EgyObj
+    {
+        public string szoveg;
+        public EgyObj(string szoveg)
+        {
+            this.szoveg = szoveg;
+        }
+        public static EgyObj operator +(EgyObj obj1, string szoveg)
+        {
+            return new EgyObj(obj1.szoveg+"\n"+szoveg);
+        }
+        public static EgyObj operator -(EgyObj obj1, string szoveg)
+        {
+            Console.WriteLine(obj1.szoveg.Except(szoveg));
+            Console.WriteLine(obj1.szoveg);
+            return obj1;
+        }
+
+    }
+}
---
+++ operatorok_gyak/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace operatorok_gyak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EgyObj szoveg1 = new EgyObj("Ez egy jó mondat.");
+            Console.WriteLine((szoveg1 + "Ez egy másik jó mondat.").szoveg);
+            Console.WriteLine((szoveg1 - "aaa").szoveg);
+
+
+        }
+    }
+}
