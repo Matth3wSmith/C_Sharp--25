3294eab7aeab239be6e6e6350b2e871d63c4bb5d
unknown <kovacs.mate.22@ady-nagyatad.lan>
2026-01-27 11:25:29 +0100

    Regex gyak

---
+++ regexp-gyak/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+using System.Threading.Channels;
+
+namespace regexp_gyak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var adathalmaz = File.ReadAllText("shakespeare.txt");
+            Regex regex = new Regex(@"^[A-Z]+\b");
+
+            Console.WriteLine(regex.Matches(adathalmaz).Count);
+        }
+    }
+}
