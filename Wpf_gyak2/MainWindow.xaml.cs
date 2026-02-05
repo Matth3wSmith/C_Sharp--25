e8266bdec817c105f7b160859b9d197262ec8532
Matth3wSmith <kovacs.mate.22@ady-nagyatad.hu>
2026-02-05 13:15:46 +0100

    WPF gyak 2

---
+++ Wpf_gyak2/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_gyak2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var szamok = new int[3] { int.Parse(Szam1.Text), int.Parse(Szam2.Text), int.Parse(Szam3.Text) };
+
+            Eredmeny.Text = szamok.OrderDescending().ToArray()[1] * szamok.OrderDescending().ToArray()[2] + ""; 
+        }
+
+        private void Valtozas(object sender, TextChangedEventArgs e)
+        {
+            int hossz = Szoveg2.Text.Length;
+            Honnan.Maximum = hossz-1;
+            Meddig.Maximum = hossz-1;
+        }
+
+        private void Honnan_TouchMove(object sender, TouchEventArgs e)
+        {
+
+        }
+    }
+}
