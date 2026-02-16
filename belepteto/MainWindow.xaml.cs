b3538a2a9bd3c643c151c5709fc7b0bf8b33f6b2
Matthew-F4 <kovacs.mate.22@F4-53>
2026-02-16 10:34:31 +0100

    Beléptető érettségi feladat, interface készítés folyamatban

--- belepteto/MainWindow.xaml.cs
+++ belepteto/MainWindow.xaml.cs
@@ -17,20 +17,23 @@ namespace belepteto
     /// </summary>
     public partial class MainWindow : Window
     {
+        int FoGridSor = 3;
+        int FoGridOszlop = 3;
         public MainWindow()
         {
             InitializeComponent();
 
             //1.feladat
-            List<List<string>> adatok = File.ReadAllLines("beadat.txt").Select(x=>x.Split().ToList()).ToList();
-            
+            //List<List<string>> adatok = File.ReadAllLines("beadat.txt").Select(x=>x.Split().ToList()).ToList();
 
+            gridTablazat(FoGrid, FoGridOszlop, FoGridSor);
+            feladatKartya(3,1,2);
         }
 
-        void feladatKartya()
+        void feladatKartya(int feladatSzam, int pozicioSor, int poziciosOszlop)
         {
             //Keret
-            Border keret = new Border
+            Border kulsoKeret = new Border
             {
                 BorderThickness = new Thickness(10),
                 CornerRadius = new CornerRadius(5),
@@ -41,15 +44,59 @@ namespace belepteto
                     StartPoint = new Point(0.5, 0),
                     GradientStops = new GradientStopCollection
                     {
-                        new GradientStop((Color)ColorConverter.ConvertFromString("#FF320A8E")),
+                        new GradientStop((Color)ColorConverter.ConvertFromString("#FF320A8E"),0),
                         new GradientStop(Colors.Orange, 0.5),
                         new GradientStop(Colors.Yellow, 1)
                     }
-
                 }
             };
 
+            Border belsoKeret = new Border
+            {
+                BorderThickness = new Thickness(1),
+                BorderBrush = Brushes.Black,
+            };
+            Grid belsoGrid = new Grid
+            {
+                Name = "Feladat"+ feladatSzam,
+            };
+            Label szoveg = new Label
+            {
+                Content = feladatSzam+". feladat",
+                FontSize = 20,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Top
+            };
+
+            Grid.SetRow(kulsoKeret, pozicioSor);
+            Grid.SetColumn(kulsoKeret, poziciosOszlop);
+
+            kulsoKeret.Child = belsoKeret;
+            belsoKeret.Child = belsoGrid;
+            belsoGrid.Children.Add(szoveg);
+            
+            FoGrid.Children.Add(kulsoKeret);
         }
 
+
+        void gridTablazat(Grid GridNev, int oszlop, int sor)
+        {
+            for(int i = 0; i < oszlop;  i++)
+            {
+                ColumnDefinition ujOszlop = new ColumnDefinition()
+                {
+                    Width = new GridLength(1, GridUnitType.Star)
+                };
+                GridNev.ColumnDefinitions.Add(ujOszlop);
+            }
+            for(int k = 0; k < sor; k++)
+            {
+                RowDefinition ujSor = new RowDefinition()
+                {
+                    Height = GridLength.Auto
+                };
+                GridNev.RowDefinitions.Add(ujSor);
+            }
+        }
     }
 }
--- wpf_gyak3/obj/Debug/net8.0-windows/wpf_gyak3.AssemblyInfo.cs
+++ wpf_gyak3/obj/Debug/net8.0-windows/wpf_gyak3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("wpf_gyak3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+481fa89d7e5497dd1ef52bc8f42af3e7a405d6c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf51df1c757853d548fd4c1ba0a55c74ef19027f")]
 [assembly: System.Reflection.AssemblyProductAttribute("wpf_gyak3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("wpf_gyak3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
