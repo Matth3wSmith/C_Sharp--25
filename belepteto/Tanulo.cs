2c1af209f26f045e11c5a9c52550e033f2197761
Matth3wSmith <kovacs.mate.22@ady-nagyatad.hu>
2026-02-27 09:00:53 +0100

    tanulo.cs

---
+++ belepteto/Tanulo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace belepteto
+{
+    class Tanulo
+    {
+        public int kod;
+        public string idoSzoveg;
+        public TimeOnly ido;
+        public string azon;
+        public Tanulo(string azon, string ido, int kod)
+        {
+            this.kod = kod;
+            this.idoSzoveg = ido;
+            this.azon = azon;
+            this.ido = TimeOnly.Parse(ido);
+        }
+
+        public string Szoveg()
+        {
+            return this.azon + " " + this.idoSzoveg + " " + this.kod;
+        }
+
+
+    }
+}
