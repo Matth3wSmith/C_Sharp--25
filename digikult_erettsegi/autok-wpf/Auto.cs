a0e5336934700b0b32c21752c014d263a4b6b546
unknown <kovacs.mate.22@ady-nagyatad.lan>
2026-03-26 11:25:29 +0100

    auto

---
+++ digikult_erettsegi/autok-wpf/Auto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace autok_wpf
+{
+    internal class Auto
+    {
+        string rendszam;
+        int ora;
+        int perc;
+        int sebesseg;
+        TimeOnly idopont;
+        //az autó rendszáma, a jeladás idejének óra, illetve perc értéke, valamint a jeladáskor mért sebesség km / h mértékegységbe
+        public Auto(string rendszam, int ora, int perc, int sebesseg) {
+            this.rendszam = rendszam;
+            this.ora= ora;
+            this.perc = perc;
+            this.sebesseg = sebesseg;
+            this.idopont = new TimeOnly(ora, perc);
+        }
+    }
+}
---
+++ digikult_erettsegi/autok-wpf/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace autok_wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //1.feladat
+            var adatok = File.ReadLines("jeladas.txt");
+            List<Auto> osszAdat = new List<Auto>();
+            Dictionary<string, List<Auto>> autok = new Dictionary<string, List<Auto>>();
+
+            foreach (var adat in adatok)
+            {
+                var split = adat.Split('\t');
+                Auto auto = new Auto(split[0], int.Parse(split[1]), int.Parse(split[2]), int.Parse(split[3]));
+                osszAdat.Add(auto);
+                if (autok.Keys.Contains(split[0]))
+                {
+                    autok[split[0]].Add(auto);
+                }
+                else
+                {
+                    autok.Add(split[0], new List<Auto>() { auto });
+                }
+            }
+
+        }
+    }
+}
