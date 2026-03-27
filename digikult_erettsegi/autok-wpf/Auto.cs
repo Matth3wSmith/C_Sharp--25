6cfdb88a921e2b02c91782c9c82c64b68ce40a18
Matth3wSmith <kovacs.mate.22@ady-nagyatad.hu>
2026-03-27 10:35:16 +0100

    3. feladat kesz

--- digikult_erettsegi/autok-wpf/Auto.cs
+++ digikult_erettsegi/autok-wpf/Auto.cs
@@ -8,11 +8,11 @@ namespace autok_wpf
 {
     internal class Auto
     {
-        string rendszam;
-        int ora;
-        int perc;
-        int sebesseg;
-        TimeOnly idopont;
+        public string rendszam;
+        public int ora;
+        public int perc;
+        public int sebesseg;
+        public TimeOnly idopont;
         //az autó rendszáma, a jeladás idejének óra, illetve perc értéke, valamint a jeladáskor mért sebesség km / h mértékegységbe
         public Auto(string rendszam, int ora, int perc, int sebesseg) {
             this.rendszam = rendszam;
@@ -20,6 +20,11 @@ namespace autok_wpf
             this.perc = perc;
             this.sebesseg = sebesseg;
             this.idopont = new TimeOnly(ora, perc);
+        }
+
+        public string RendszamNelkul()
+        {
+            return "Mérés időpontja: "+ this.idopont.ToString() + ", Sebessége: " + this.sebesseg+ " km/h";
         }
     }
 }
--- digikult_erettsegi/autok-wpf/MainWindow.xaml.cs
+++ digikult_erettsegi/autok-wpf/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.IO;
+using System.Text;
+using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,8 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.IO;
-
+using System.Windows.Threading;
 namespace autok_wpf
 {
     /// <summary>
@@ -17,14 +18,18 @@ namespace autok_wpf
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DispatcherTimer timer;
+        List<Auto> osszAdat = new List<Auto>();
+        Dictionary<string, List<Auto>> autok = new Dictionary<string, List<Auto>>();
+        //2. feladat változója
+        TimeOnly utolsoIdopont;
+        TimeOnly kezdoIdopont;
         public MainWindow()
         {
             InitializeComponent();
 
             //1.feladat
             var adatok = File.ReadLines("jeladas.txt");
-            List<Auto> osszAdat = new List<Auto>();
-            Dictionary<string, List<Auto>> autok = new Dictionary<string, List<Auto>>();
 
             foreach (var adat in adatok)
             {
@@ -40,7 +45,47 @@ namespace autok_wpf
                     autok.Add(split[0], new List<Auto>() { auto });
                 }
             }
+        }
+        //2. feladat
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Utolsó jeladás időpontja
+            TimeOnly utolso = osszAdat[osszAdat.Count - 1].idopont;
+            Rendszam.Text += " " +osszAdat[osszAdat.Count - 1].rendszam;
+            utolsoIdopont = utolso;
+            kezdoIdopont = new TimeOnly(0,0,0);
+            timer = new DispatcherTimer();
 
+            timer.Interval = TimeSpan.FromMilliseconds(0.5);
+
+            timer.Tick += Timer_Tick;   
+
+            timer.Start();
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (utolsoIdopont == kezdoIdopont)
+            {
+                timer.Stop();
+                timer.Tick -= Timer_Tick;
+            }
+            Clock.Text = kezdoIdopont.ToString("HH:mm:ss");
+            kezdoIdopont = kezdoIdopont.Add(TimeSpan.FromSeconds(1));
+            
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            timer.Stop();
+            timer.Tick -= Timer_Tick;
+            base.OnClosed(e);
+        }
+
+        private void Feladat_3(object sender, RoutedEventArgs e)
+        {
+            var elsoAutoAdat = autok[osszAdat[0].rendszam];
+            RendszamLista.ItemsSource = elsoAutoAdat.Select(x => x.RendszamNelkul());
+            Rendszam3.Text = elsoAutoAdat[0].rendszam;
         }
     }
 }
