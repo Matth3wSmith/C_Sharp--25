f04ace7b279a79a364162cd9dfeaa2bced5af6ae
Matth3wSmith <kovacs.mate.22@ady-nagyatad.hu>
2026-03-31 11:29:52 +0200

    Dolgozat

---
+++ kraterek dolgozat/Krater.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace kraterek_dolgozat
+{
+    internal class Krater
+    {
+        public double x;
+        public double y;
+        public double r;
+        public string nev;
+        public Krater(double x, double y, double r, string nev)
+        {
+            this.nev = nev;
+            this.x = x;
+            this.y = y;
+            this.r = r;
+        }
+        public string Adatok()
+        {
+            //„A(z) Stephen Hawking középpontja X=3.45 Y = 2.78 sugara R = 0.35.
+            return $"A(z) {this.nev} középpontja X={this.x} Y = {this.y} sugara R = {this.r}.";
+        }
+
+        //Függvény tavolsag(x1, y1, x2, y2 : Valós ) : Valós 
+        //tavolsag := Négyzetgyök((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)) 
+        //Függvény vége
+        public double tavolsag(double x2, double y2)
+        {
+            return Math.Sqrt((x2 - this.x) * (x2 - this.x) + (y2 - this.y) * (y2 - this.y));
+        }
+        public string tartalmaz(string nev)
+        {
+            return $"A(z) {this.nev} kráter tartalmazza a(z) {nev} krátert.";
+        }
+    }
+}
---
+++ kraterek dolgozat/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace kraterek_dolgozat
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Krater> kraterek;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var adatok = File.ReadLines("felszin_tvesszo.txt");
+            kraterek = adatok.Select(x =>
+            {
+                var split = x.Split('\t');
+                return new Krater(double.Parse(split[0]), double.Parse(split[1]), double.Parse(split[2]), split[3]);
+
+            }).ToList();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            KraterSzam.Text = kraterek.Count+"";
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string nev = KraterNev.Text;
+            var krater = kraterek.FirstOrDefault(x => x.nev == nev);
+            if (krater == null)
+            {
+                NevShow.Text = "Nincs ilyen nevű kráter!";
+            }
+            else
+            {
+                NevShow.Text = krater.Adatok();
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            var max = kraterek.Max(x => x.r);
+            var maxok = kraterek.Where(x => x.r == max);
+            Feladat4.ItemsSource = maxok.Select(x => x.Adatok());
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            var kertnev = Nev.Text;
+            Krater krater = kraterek.Where(x => x.nev == kertnev).First();
+            Nincskozos.ItemsSource = kraterek.Where(x => x.tavolsag(krater.x, krater.y) > (krater.r + x.r) && x.nev!=krater.nev).Select(x=>x.nev);
+            
+    
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            List<string> nevek = new List<string>();
+            foreach (var item1 in kraterek)
+            {
+                foreach (var item2 in kraterek)
+                {
+                    if(item1!=item2 && item1.tavolsag(item2.x,item2.y) < Math.Max(item2.r, item1.r))
+                    {
+
+                        if (item1.r > item2.r )
+                        {
+                            nevek.Add(item1.tartalmaz(item2.nev));
+                        }
+                        else
+                        {
+                            nevek.Add(item2.tartalmaz(item1.nev));
+                        }
+                    }
+                }
+            }
+            Tartalmaz.ItemsSource = nevek;
+        }
+    }
+}
