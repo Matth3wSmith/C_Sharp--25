debcea71dab90f73cd40393e3190d11e092ffed9
unknown <kovacs.mate.22@ady-nagyatad.lan>
2026-04-13 10:35:20 +0200

    szállítószalag

---
+++ digikult_erettsegi/szalag/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace szalag
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //1. feladat Fájlbeolvasás
+            var adatok = File.ReadAllLines("szallit.txt", Encoding.UTF8);
+            int szalagHossz = int.Parse(adatok[0].Split(" ")[0]);
+            int szalagSzelesseg = int.Parse(adatok[0].Split(" ")[1]);
+            List<Rekesz> rekeszek = new List<Rekesz>();
+            for(int i = 1; i < adatok.Length; i++)
+            {
+                var split = adatok[i].Split(" ");
+                rekeszek.Add(new Rekesz(i + 1, int.Parse(split[0]), int.Parse(split[1]), int.Parse(split[2]), int.Parse(split[3]), szalagHossz));
+            }
+
+
+        }
+    }
+}
---
+++ digikult_erettsegi/szalag/Rekesz.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szalag
+{
+    internal class Rekesz
+    {
+        public int ido;
+        public int kezd;
+        public int veg;
+        public int tomeg;
+        public int hossz;
+        public int szalagHossz;
+        public int sorszam;
+        public Rekesz(int sorszam, int ido, int kezd, int veg, int tomeg, int szalagHossz)
+        {
+            this.sorszam = sorszam;
+            this.ido = ido;
+            this.kezd = kezd;
+            this.veg = veg;
+            this.tomeg = tomeg;
+            this.szalagHossz = szalagHossz;
+            if (kezd > veg)
+            {
+                this.hossz = veg - kezd;
+            }
+            else
+            {
+                this.hossz = szalagHossz - kezd + veg;
+            }
+        }
+    }
+}
